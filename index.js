54779da5ec0392f8e71992c5a94e529d00b40343
KlaasNiphaas <80031535+KlaasNiphaas@users.noreply.github.com>
2021-09-17 15:07:28 +0200

    Cleaned up your code

--- index.js
+++ index.js
@@ -3,38 +3,37 @@ const cmd = require('mineflayer-cmd').plugin
 const fs = require('fs');
 let rawdata = fs.readFileSync('config.json');
 let data = JSON.parse(rawdata);
-var lasttime = -1;
-var moving = 0;
-var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
-var lastaction;
-var pi = 3.14159;
-var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var username = data["name"]
-var nightskip = data["auto-night-skip"]
-var bot = mineflayer.createBot({
+let lasttime = -1;
+let moving = 0;
+let connected = 0;
+let actions = [ 'forward', 'back', 'left', 'right']
+let lastaction;
+let pi = Math.PI;
+let moveinterval = 2; // 2 second movement interval
+let maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+let host = data["ip"];
+let username = data["name"]
+let nightskip = data["auto-night-skip"]
+
+const bot = mineflayer.createBot({
   host: host,
   username: username
 });
-function getRandomArbitrary(min, max) {
-       return Math.random() * (max - min) + min;
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 bot.loadPlugin(cmd)
 
-
-
-bot.on('login',function(){
+bot.on('login', function() {
 	console.log("Logged In")
 	bot.chat("hello");
 });
 
 bot.on('time', function(time) {
-	if(nightskip == "true"){
-	if(bot.time.timeOfDay >= 13000){
+	if (nightskip == "true") {
+	if (bot.time.timeOfDay >= 13000) {
 	bot.chat('/time set day')
 	}}
     if (connected <1) {
@@ -43,16 +42,16 @@ bot.on('time', function(time) {
     if (lasttime<0) {
         lasttime = bot.time.age;
     } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
+        let randomadd = Math.random() * maxrandom * 20;
+        let interval = moveinterval * 20 + randomadd;
         if (bot.time.age - lasttime > interval) {
             if (moving == 1) {
                 bot.setControlState(lastaction,false);
                 moving = 0;
                 lasttime = bot.time.age;
             } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
+                let yaw = Math.random() * pi - (0.5 * pi);
+                let pitch = Math.random() * pi - (0.5 * pi);
                 bot.look(yaw,pitch,false);
                 lastaction = actions[Math.floor(Math.random() * actions.length)];
                 bot.setControlState(lastaction,true);
@@ -71,4 +70,3 @@ bot.on('spawn',function() {
 bot.on('death',function() {
     bot.emit("respawn")
 });
-
